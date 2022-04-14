c4199b6da8b8413244719cdff0eae0ea08d2080d
gameboyJ3 <jatin.collabera1411@gmail.com>
2022-04-14 15:54:35 +0530

    v1
    v1

---
+++ js/data.js
@@ -0,0 +1,203 @@
+var dataJson = {
+    "scenes": [{
+            "sceneName": "1",
+            "image": "1.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "2",
+                    "infoPointsCoordinates": [-4079.22, -2384.44, -1609.02],
+                    "infoHoverText": "2",
+                }]
+        }, {
+            "sceneName": "2",
+            "image": "2.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "1",
+                    "infoPointsCoordinates": [-69.59, -1336.17, 4810.56],
+                    "infoHoverText": "1"
+                }, {
+                    "infoPointsName": "3",
+                    "infoPointsCoordinates": [-3793.74, -1796.61, -2707.11],
+                    "infoHoverText": "3"		
+                }
+            ]
+        }, {
+            "sceneName": "3",
+            "image": "3-1.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "2",
+                    "infoPointsCoordinates": [-3106.89, -821.01, 3820.04],
+                    "infoHoverText": "2"					
+                }, {
+                    "infoPointsName": "4",
+                    "infoPointsCoordinates": [-3793.74, -1796.61, -2707.11],
+                    "infoHoverText": "4"		
+                }
+            ]
+        },
+		{
+            "sceneName": "4",
+            "image": "3-2.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "3",
+                    "infoPointsCoordinates": [-3106.89, -821.01, 3820.04],
+                    "infoHoverText": "3"					
+                }, {
+                    "infoPointsName": "5",
+                    "infoPointsCoordinates": [4448.56, -889.24, -2085.30],
+                    "infoHoverText": "5"		
+                }
+            ]
+        }, 
+		{
+            "sceneName": "5",
+            "image": "4.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "4",
+                    "infoPointsCoordinates": [-3106.89, -821.01, 3820.04],
+                    "infoHoverText": "4"					
+                }, {
+                    "infoPointsName": "6",
+                    "infoPointsCoordinates": [4448.56, -889.24, -2085.30],
+                    "infoHoverText": "6"		
+                }
+            ]
+        }, 
+		{
+            "sceneName": "6",
+            "image": "5.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "5",
+                    "infoPointsCoordinates": [1044.28, -304.09, -4877.12],
+                    "infoHoverText": "5"					
+                }, {
+                    "infoPointsName": "7",
+                    "infoPointsCoordinates": [-719.98, -555.77, 4906.21],
+                    "infoHoverText": "7"		
+                }
+            ]
+        }, 
+		{
+            "sceneName": "7",
+            "image": "6.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "6",
+                    "infoPointsCoordinates": [-723.13, -930.40, 4848.80],
+                    "infoHoverText": "6"					
+                }, {
+                    "infoPointsName": "8",
+                    "infoPointsCoordinates": [-1112.77, 53.12, -4868.96],
+                    "infoHoverText": "8"		
+                }
+            ]
+        }
+		, 
+		{
+            "sceneName": "8",
+            "image": "7.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "7",
+                    "infoPointsCoordinates": [4643.79, -782.30, 1663.93],
+                    "infoHoverText": "7"					
+                }, {
+                    "infoPointsName": "9",
+                    "infoPointsCoordinates": [-723.13, -930.40, 4848.80],
+                    "infoHoverText": "9"		
+                }
+            ]
+        }
+		, 
+		{
+            "sceneName": "9",
+            "image": "8.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "8",
+                    "infoPointsCoordinates": [-3792.93, -1391.02, 2930.94],
+                    "infoHoverText": "8"					
+                }, {
+                    "infoPointsName": "10",
+                    "infoPointsCoordinates": [-4273.30, 476.51, -2540.97],
+                    "infoHoverText": "10"		
+                }
+            ]
+        }
+		, 
+		{
+            "sceneName": "10",
+            "image": "9.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "9",
+                    "infoPointsCoordinates": [2965.40, -667.77, -3961.95],
+                    "infoHoverText": "9"					
+                }, {
+                    "infoPointsName": "11",
+                    "infoPointsCoordinates": [3714.54, -675.30, 3269.51],
+                    "infoHoverText": "11"		
+                }
+            ]
+        }
+		, 
+		{
+            "sceneName": "11",
+            "image": "10.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "10",
+                    "infoPointsCoordinates": [3714.54, -675.30, 3269.51],
+                    "infoHoverText": "9"					
+                }, {
+                    "infoPointsName": "12",
+                    "infoPointsCoordinates": [2965.40, -667.77, -3961.95],
+                    "infoHoverText": "12"		
+                }				
+				
+            ]
+        }
+		, 
+		{
+            "sceneName": "12",
+            "image": "11.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "11",
+                    "infoPointsCoordinates": [3714.54, -675.30, 3269.51],
+                    "infoHoverText": "11"					
+                }, {
+                    "infoPointsName": "13",
+                    "infoPointsCoordinates": [2965.40, -667.77, -3961.95],
+                    "infoHoverText": "13"		
+                }				
+				
+            ]
+        }
+		, 
+		{
+            "sceneName": "13",
+            "image": "12.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "12",
+                    "infoPointsCoordinates": [3714.54, -675.30, 3269.51],
+                    "infoHoverText": "12"					
+                }, {
+                    "infoPointsName": "14",
+                    "infoPointsCoordinates": [-2389.34, -2127.16, -3831.46],
+                    "infoHoverText": "14"		
+                }				
+				
+            ]
+        }
+		, 
+		{
+            "sceneName": "14",
+            "image": "13.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "13",
+                    "infoPointsCoordinates": [3714.54, -675.30, 3269.51],
+                    "infoHoverText": "13"					
+                }, {
+                    "infoPointsName": "15",
+                    "infoPointsCoordinates": [-2389.34, -2127.16, -3831.46],
+                    "infoHoverText": "15"		
+                }				
+				
+            ]
+        }
+    ]
+};
